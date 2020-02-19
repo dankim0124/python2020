587fe1dfce32e0d1b50ac7867884c49f93535e30
dankim <dankim0124@gmail.com>
2020-02-19 16:59:31 -0600

    Add files via upload

---
+++ untitled/emailExtractor.py
@@ -0,0 +1,38 @@
+#Email_Extractor.py
+#Name:dohyun kim
+#Date:2020/02/19
+#Assignment:email exracting
+
+import urllib.request
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+from re import *
+
+def main():
+
+    #prompt the user for a webpage url
+    #url = 'https://www.unomaha.edu/college-of-information-science-and-technology/about/faculty-staff/index.php'
+    print("if it does not pass to input URL and pop-up in chrome browser, add white-space in the end of the input URL")
+    try:
+        url = str(input("input URL : "))
+        url+=" "
+        url_html = urllib.request.urlopen(url)
+    except HTTPError:
+        print(HTTPError)
+        print("unusable URL : maybe because of blocking function in target wesite....")
+        print("it might be blocked with 404 error when you access web need log-in")
+        return
+    soup = BeautifulSoup(url_html, "html.parser")
+
+    #Convert the soup to a string version
+    soup = str(soup)
+
+    reg1 = r"\w+@\w+\W\w+\b"
+    emails = findall(reg1,soup)
+
+    emails = list(set(emails))
+    for elem in emails:
+        print(elem)
+
+main()
