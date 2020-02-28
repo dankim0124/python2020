3e4585c10af6ab742165c57b2026fe48e2b0c715
dankim <dankim0124@gmail.com>
2020-02-28 16:23:58 -0600

    Add files via upload

--- untitled/emailExtractor.py
+++ untitled/emailExtractor.py
@@ -28,11 +28,23 @@ def main():
     #Convert the soup to a string version
     soup = str(soup)
 
-    reg1 = r"\w+@\w+\W\w+\b"
-    emails = findall(reg1,soup)
+    reg1 = r"\w+@\w+\.\w+\b"
+    reg2 = r"\w+@\w+\.\w+\.\w+\b"
+
+    emails = findall(reg1, soup)
+    emails2 = findall(reg2,soup)
+
+    # delete redundancy , there's too many same email in test site.
+    emails += emails2
+
 
     emails = list(set(emails))
-    for elem in emails:
-        print(elem)
+
+    if emails:
+        print("Found Emails below.. ")
+        for elem in emails:
+            print(elem)
+    else:
+        print("NO email strings. in this web...")
 
 main()
